7429e812368b0e8395648aa6834681e7d59e6282
Abelardo <37087157+avillalobosd@users.noreply.github.com>
2021-06-20 15:39:15 -0500

    Add files via upload

--- proyecto/back/app.js
+++ proyecto/back/app.js
@@ -3,15 +3,15 @@ const app = express();
 const sequelize = require('./database/db');
 const BD = require('./database/models/User');
 var cors = require('cors');
-
+const Seq=require('sequelize')
 // Setting
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3000;
+const Op = Seq.Op;
 
 //cors
 app.use(cors());
 
-var whitelist = ['http://localhost:3000']
+var whitelist = ['http://localhost:3000', '*']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -21,103 +21,219 @@ var corsOptions = {
     }
   }
 }
+app.use(express.json());
 
 
 // Rutas
 app.get('/', function (req, res) {
 
-    BD.create({
-        name: "OK",
-        birthday: new Date(1999, 4, 6)
-    }).then(user => {
-        res.json(user);
-    });
+  BD.create({
+    name: "OK",
+    birthday: new Date(1999, 4, 6)
+  }).then(user => {
+    res.json(user);
+  });
 
-    // User.findAll().then(users => {
-    //     res.json(users);
-    // });
+  // User.findAll().then(users => {
+  //     res.json(users);
+  // });
 
 });
 app.get('/chuchoapi/crea', function (req, res) {
 
-    BD.create({
-        nombre: "Pepe",
-        telefono: "8116360604"
-    }).then(user => {
-        res.json(user);
-    });
+  BD.create({
+    nombre: "Jesús Manuel",
+    ap_paterno: "Ortiz",
+    ap_materno: "Morales",
+    no_emp: "00000",
+    sexo: "Masculino",
+    municipio: "García",
+    escuela: "SNTE",
+    region: "1",
+    telefono: "8116360604",
+    activojubilado: "activo",
+    nivel: "Primaria",
+    foto: "http://",
+    referido: 0,
+    agregados: 0,
+    verificado: true
+
+  }).then(user => {
+    { }
+    res.json(user);
+  });
 });
 
 app.get('/chuchoapi/muestra', function (req, res) {
 
-    BD.findAll().then(users => {
-        res.json(users);
-    });
+  BD.findAll().then(users => {
+    res.json(users);
+  });
 
 });
 
-app.get("/chuchoapi/TelefonoOne/:id", (req, res) => {
-    console.log(req.params.id);
-    BD.findOne({
-      where:{
-        telefono: req.params.id
-      }
-      
-    })
-      .then(data => {
-        res.json({
-          id: data.id,
-          nombre: data.nombre, 
-          ap_paterno:data.ap_paterno,
-          ap_materno: data.ap_materno,
-          telefono: data.telefono,
-          password:data.password,
-          status:"EXITO"
-        })
-      }).catch(function (err) {
-        res.json({
-            status: "ERROR"
-          })
-      });;  
-  });
 
-  app.get("/chuchoapi/ClavePass/:id", (req, res) => {
-    console.log(req.params.id);
-    BD.findOne({
-      where:{
-        password: req.params.id
+app.get('/chuchoapi/muestraHijos/:idPadre', function (req, res) {
+
+  BD.findAll({
+    where: {
+      cadena: {
+        [Op.like]: req.params.idPadre+'%'
       }
-    })
-      .then(data => {
-        res.json({
-          id: data.id,
-          nombre: data.nombre, 
-          ap_paterno:data.ap_paterno,
-          ap_materno: data.ap_materno,
-          telefono: data.telefono,
-          password:data.password,
-          status:"EXITO"
-        })
-      }).catch(function (err) {
-        res.json({
-            status: "ERROR"
-          })
-      });;
-    
-  
+    }
+  }).then(users => {
+    res.json(users);
   });
+});
+// BUSCA EL TELEFONO EN LA BASE DE DATOS 
+app.get("/chuchoapi/TelefonoOne/:id", (req, res) => {
+  console.log(req.params.id);
+  BD.findOne({
+    where: {
+      telefono: req.params.id
+    }
+
+  })
+    .then(data => {
+      res.json({
+        id: data.id,
+        nombre: data.nombre,
+        ap_paterno: data.ap_paterno,
+        ap_materno: data.ap_materno,
+        telefono: data.telefono,
+        password: data.password,
+        status: "EXITO"
+      })
+    }).catch(function (err) {
+      res.json({
+        status: "ERROR"
+      })
+    });;
+});
+
+// BUSCA EL NO_EMPLEADO EN LA BASE DE DATOS 
+app.get("/chuchoapi/NoEmpleado/:id", (req, res) => {
+  console.log(req.params.id);
+  BD.findOne({
+    where: {
+      no_emp: req.params.id
+    }
+
+  })
+    .then(data => {
+      res.json({
+        id: data.id,
+        nombre: data.nombre,
+        ap_paterno: data.ap_paterno,
+        ap_materno: data.ap_materno,
+        telefono: data.telefono,
+        password: data.password,
+        no_emp: data.no_emp,
+        status: "EXITO"
+      })
+    }).catch(function (err) {
+      res.json({
+        status: "ERROR"
+      })
+    });;
+});
+
+
+app.put("/chuchoapi/registrar", (request, res) => {
+  BD.create({
+    nombre: request.body.nombre,
+    ap_paterno: request.body.ap_paterno,
+    ap_materno: request.body.ap_materno,
+    no_emp: request.body.no_emp,
+    // no_emp: "FL0021",
+    sexo: request.body.sexo,
+    municipio: request.body.municipio,
+    escuela: request.body.escuela,
+    region: request.body.region,
+    telefono: request.body.telefono,
+    activojubilado: request.body.activojubilado,
+    nivel: request.body.nivel,
+    foto: request.body.foto,
+    password: request.body.password,
+    referido: request.body.referido,
+    agregados: 0,
+    verificado: false,
+    cadena:request.body.cadena,
+    hijos:false
+  }).then(data => {
+    res.json({
+      status: "EXITO"
+    })
+  }).catch(function (err) {
+    res.json({
+      status: "ERROR",
+      error: err
+    })
+  });;
+})
+
+// BUSCA LA CONTRESEÑA EN LA BASE DE DATOS 
+app.get("/chuchoapi/ClavePass/:id", (req, res) => {
+  console.log(req.params.id);
+  BD.findOne({
+    where: {
+      password: req.params.id
+    }
+  })
+    .then(data => {
+      res.json({
+        id: data.id,
+        nombre: data.nombre,
+        ap_paterno: data.ap_paterno,
+        ap_materno: data.ap_materno,
+        telefono: data.telefono,
+        password: data.password,
+        municipio: data.municipio,
+        escuela: data.escuela,
+        region: data.region,
+        activojubilado: data.activojubilado,
+        nivel: data.nivel,
+        foto: data.foto,
+        agregados: data.agregados,
+        status: "EXITO",
+        hijos:data.hijos,
+        cadena:data.cadena
+      })
+    }).catch(function (err) {
+      res.json({
+        status: "ERROR"
+      })
+    });;
+
+
+});
+
+app.get("/chuchoapi/hijos/:id", (req, res) => {
+  BD.update({hijos:true}, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function (dbUser) {
+    res.json({mensaje:"ACTUALIZADO HIJO"})
+  })
+    .catch(function (err) {
+      console.log(err)
+      console.log(err)
+      res.json({mensaje:"NO ACTUALIZADO HIJO"})
+    })
+})
 
 
 // Arrancamos el servidor
 app.listen(PORT, function () {
-    console.log(`La app ha arranado en http://localhost:${PORT}`);
+  console.log(`La app ha arranado en http://localhost:${PORT}`);
 
-    // Conectase a la base de datos
-    // Force true: DROP TABLES
-    sequelize.sync({ force: false }).then(() => {
-        console.log("Nos hemos conectado a la base de datos");
-    }).catch(error => {
-        console.log('Se ha producido un error', error);
-    })
+  // Conectase a la base de datos
+  // Force true: DROP TABLES
+  sequelize.sync({ force: false }).then(() => {
+    console.log("Nos hemos conectado a la base de datos");
+  }).catch(error => {
+    console.log('Se ha producido un error', error);
+  })
 
 });
--- proyecto/back/database/models/User.js
+++ proyecto/back/database/models/User.js
@@ -6,17 +6,22 @@ User.init({
     nombre: DataTypes.STRING,
     ap_paterno: DataTypes.STRING,
     ap_materno: DataTypes.STRING,
+    no_emp: {type: DataTypes.STRING, unique: true},
+    sexo: DataTypes.STRING,
+    municipio: DataTypes.STRING,
     escuela: DataTypes.STRING,
     region: DataTypes.STRING,
     telefono: {type: DataTypes.STRING, unique: true},
-    activo: DataTypes.STRING,
-    jubilado: DataTypes.STRING,
+    activojubilado: DataTypes.STRING,
     nivel: DataTypes.STRING,
     foto: DataTypes.STRING,
     primeraVez: {type:DataTypes.BOOLEAN, defaultValue: false}, 
-    password: DataTypes.STRING,
+    password: {type:DataTypes.STRING, defaultValue: "0"}, 
     referido: DataTypes.INTEGER,
-    agregados: DataTypes.INTEGER
+    agregados: DataTypes.INTEGER,
+    verificado: DataTypes.BOOLEAN,
+    hijos: {type:DataTypes.BOOLEAN, defaultValue: false},
+    cadena: DataTypes.TEXT('long')
     
 }, {
     sequelize,
